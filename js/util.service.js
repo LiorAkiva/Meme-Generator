1685d87f52a4142975ab1bdc66f651b101712031
LiorAki <lior21231@gmail.com>
2021-12-16 11:29:35 +0200

    My first commit

---
+++ js/util.service.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function saveToStorage(key, val) {
+    const json = JSON.stringify(val)
+    localStorage.setItem(key, json)
+}
+
+function loadFromStorage(key) {
+    const json = localStorage.getItem(key)
+    const val = JSON.parse(json)
+    return val
+}
+
+
+function makeId(length = 6) {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var txt = '';
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
+}
